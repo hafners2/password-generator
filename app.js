c4c7eb99ab471fa014527e97c46678cbe6ec38a2
Sabrina Hafner <hafners2@mailbox.winthrop.edu>
2024-02-22 19:52:18 -0500

    fully functional password generator

---
+++ app.js
@@ -0,0 +1,26 @@
+const passwordBox = document.getElementById("password-container");
+const length = 12;
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const num = "0123456789";
+const symbol = "@#$%^&*()_+~|{}[]<>/-=";
+const allChars = uppercase + lowercase + num + symbol;
+
+function createPassword(){
+    let password = "";
+    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    password += num[Math.floor(Math.random() * num.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+    alert("Password Copied!")
+}
